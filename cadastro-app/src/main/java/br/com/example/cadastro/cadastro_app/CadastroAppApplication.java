77bd3e95017fd185514b6d055641f048ec207cf4
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2024-06-26 11:50:32 -0300

    Primeiro commit do projeto Cadastro de funcionarios

---
+++ cadastro-app/src/main/java/br/com/example/cadastro/cadastro_app/CadastroAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.example.cadastro.cadastro_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroAppApplication.class, args);
+	}
+
+}
